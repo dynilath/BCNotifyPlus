facdc547499e35120a707675ffbd8ea2965feb6d
dynilath <dynilath@qq.com>
2024-01-15 12:16:45 +0800

    Update loader

--- src/loader.user.js
+++ src/loader.user.js
@@ -4,18 +4,19 @@
 // @version 1.0
 // @description Improved BC notification.
 // @author Saki Saotome
-// @include /^https:\/\/(www\.)?bondage(?:projects\.elementfx|-europe)\.com\/R\d+\/(BondageClub|\d+)(\/)?(((index|\d+)\.html)?)?$/
+// @match bondageprojects.elementfx.com/*
+// @match www.bondageprojects.elementfx.com/*
+// @match bondage-europe.com/*
+// @match www.bondage-europe.com/*
 // @icon  https://dynilath.gitlab.io/SaotomeToyStore/favicon.ico
-// @homepage https://dynilath.gitlab.io/SaotomeToyStore/PluginDocuments/NotifyPlus/
 // @grant none
 // @run-at document-end
 // ==/UserScript==
 
 (function () {
     "use strict";
-    if (typeof BCMoanerReloaded_Loaded === "undefined") {
-        const script = document.createElement("script");
-        script.src = `https://dynilath.gitlab.io/SaotomeToyStoreVendor/NotifyPlus/main.js?v=${Date.now()}`;
-        document.head.appendChild(script);
-    }
+    const src = `https://dynilath.gitlab.io/SaotomeToyStoreVendor/NotifyPlus/main.js?v=${Date.now()}`;
+    const loadScript = (url, okay, failed) => fetch(url).then(r => {if(r.ok) return r.text();
+        else throw new Error("Failed to load script")} ).then(okay).catch(() => { setTimeout(() => {failed(url, okay, failed)}, 5000); });
+    loadScript(src, text => {if (typeof BCNotifyPlus_Loaded === "undefined") eval(text);}, loadScript);
 })();
