da220d85e247046c083ae9b68b377a11f0c279ea
dynilath <dynilath@qq.com>
2025-04-20 00:43:05 +0800

    refactor: convert CN and EN exports to object literals for improved type safety

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,12 @@
-import { CN } from "./CN";
+import { CN, TextKey } from "./CN";
 import { EN } from "./EN";
 
-export function GetText(srcTag: string, fill: any = {}) {
+export function GetText(srcTag: TextKey, fill: any = {}) {
     let ret = "";
     if (TranslationLanguage === 'CN') {
-        ret = CN.get(srcTag) || srcTag;
+        ret = CN[srcTag] || srcTag;
     }
-    else ret = EN.get(srcTag) || srcTag;
-
+    else ret = EN[srcTag] || srcTag;
     for (const k in fill) {
         ret = ret.replace(k, fill[k])
     }
